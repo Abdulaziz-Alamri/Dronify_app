35460c0e2c10ad099862f029c7ce7df5609a4406
rawan <rawan1993_7@hotmail.com>
2024-10-29 10:38:18 +0300

    edit

--- apps/dronify_mngmt/lib/main.dart
+++ apps/dronify_mngmt/lib/main.dart
@@ -1,10 +1,6 @@
-import 'package:dronify_mngmt/Admin/All_employees/all_emp.dart';
-import 'package:dronify_mngmt/Admin/Admin_Home/admin_home.dart';
-import 'package:dronify_mngmt/Admin/live_chat/chat_screen.dart';
-import 'package:dronify_mngmt/Bottom_Nav/bottom_nav.dart';
+
 import 'package:dronify_mngmt/Employee_Home/employee_home.dart';
-import 'package:dronify_mngmt/Employee_Order/confirm_screen.dart';
-import 'package:dronify_mngmt/Employee_Order/order_screen.dart';
+
 import 'package:dronify_mngmt/utils/setup.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
@@ -22,7 +18,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Sizer(
       builder: (context, orientation, deviceType) {
-        return MaterialApp(
+        return const MaterialApp(
             debugShowCheckedModeBanner: false, home: EmployeeHome());
       },
     );
