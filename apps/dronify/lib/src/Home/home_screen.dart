296174b7b4b7c702c6423abfd18bee134ad7bd3a
muhannad <mohndalzaher@gmail.com>
2024-11-04 13:02:10 +0300

    animtion

--- apps/dronify/lib/src/Home/home_screen.dart
+++ apps/dronify/lib/src/Home/home_screen.dart
@@ -6,12 +6,52 @@ import 'package:dronify/utils/setup.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late AnimationController _fadeController;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _fadeController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.3),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+
+    _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _fadeController,
+        curve: Curves.easeIn,
+      ),
+    );
+
+    _controller.forward();
+    _fadeController.forward();
+  }
 
   Future<String> fetchUserName() async {
     final dataLayer = locator<DataLayer>();
-
     final userId = dataLayer.supabase.auth.currentUser?.id;
     if (userId != null) {
       await dataLayer.getCustomer(userId);
@@ -77,54 +117,68 @@ class HomeScreen extends StatelessWidget {
                     return Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Align(
-                          alignment: Alignment.topCenter,
-                          child: WelcomeCard(
-                            name: userName,
+                        SlideTransition(
+                          position: _slideAnimation,
+                          child: Align(
+                            alignment: Alignment.topCenter,
+                            child: WelcomeCard(
+                              name: userName,
+                            ),
                           ),
                         ),
                         const SizedBox(height: 15),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16),
-                          child: Text(
-                            'Special Offers',
-                            style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xff172B4D)),
+                        FadeTransition(
+                          opacity: _fadeAnimation,
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 16),
+                            child: Text(
+                              'Special Offers',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xff172B4D)),
+                            ),
                           ),
                         ),
                         const SizedBox(height: 20),
                         Center(
-                          child: const SpecialOfferCard(
-                            imageUrl: 'assets/Group_34606-removebg-preview.png',
-                            title: 'Offer Cleaning Service',
-                            description: 'Get 25%',
+                          child: FadeTransition(
+                            opacity: _fadeAnimation,
+                            child: const SpecialOfferCard(
+                              imageUrl:
+                                  'assets/Group_34606-removebg-preview.png',
+                              title: 'Offer Cleaning Service',
+                              description: 'Get 25%',
+                            ),
                           ),
                         ),
                         const SizedBox(height: 10),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16),
-                          child: Text(
-                            'Services',
-                            style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xff172B4D)),
+                        FadeTransition(
+                          opacity: _fadeAnimation,
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 16),
+                            child: Text(
+                              'Services',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xff172B4D)),
+                            ),
                           ),
                         ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Center(
-                          child: ServiceCard(
-                            serviceId: 1,
-                            imageUrl:
-                                'assets/Dasu-pulizia-facciata-con-drone-Milano 1.png',
-                            title: 'Building Cleaning',
-                            description:
-                                'Professional cleaning service for tall buildings using advanced drones.',
-                            iconPath: 'assets/Vector (12).png',
+                        const SizedBox(height: 10),
+                        SlideTransition(
+                          position: _slideAnimation,
+                          child: Center(
+                            child: ServiceCard(
+                              serviceId: 1,
+                              imageUrl:
+                                  'assets/Dasu-pulizia-facciata-con-drone-Milano 1.png',
+                              title: 'Building Cleaning',
+                              description:
+                                  'Professional cleaning service for tall buildings using advanced drones.',
+                              iconPath: 'assets/Vector (12).png',
+                            ),
                           ),
                         ),
                         Padding(
@@ -142,22 +196,28 @@ class HomeScreen extends StatelessWidget {
                                       mainAxisSpacing: 20,
                                       crossAxisSpacing: 40,
                                       mainAxisExtent: 180),
-                              children: const [
-                                ServiceCard(
-                                  serviceId: 2,
-                                  imageUrl: 'assets/nano.jpg',
-                                  title: 'Nano Protection',
-                                  description:
-                                      'Nano-coating protection for windows, shields from dirt and weather damage.',
-                                  iconPath: 'assets/Group (1).png',
+                              children: [
+                                SlideTransition(
+                                  position: _slideAnimation,
+                                  child: const ServiceCard(
+                                    serviceId: 2,
+                                    imageUrl: 'assets/nano.jpg',
+                                    title: 'Nano Protection',
+                                    description:
+                                        'Nano-coating protection for windows, shields from dirt and weather damage.',
+                                    iconPath: 'assets/Group (1).png',
+                                  ),
                                 ),
-                                ServiceCard(
-                                  serviceId: 3,
-                                  imageUrl: 'assets/spot.jpg',
-                                  title: 'Spot Painting',
-                                  description:
-                                      'Spot painting services for building exteriors.',
-                                  iconPath: 'assets/Group (2).png',
+                                SlideTransition(
+                                  position: _slideAnimation,
+                                  child: const ServiceCard(
+                                    serviceId: 3,
+                                    imageUrl: 'assets/spot.jpg',
+                                    title: 'Spot Painting',
+                                    description:
+                                        'Spot painting services for building exteriors.',
+                                    iconPath: 'assets/Group (2).png',
+                                  ),
                                 ),
                               ],
                             ),
@@ -174,5 +234,12 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _fadeController.dispose();
+    super.dispose();
   }
 }
